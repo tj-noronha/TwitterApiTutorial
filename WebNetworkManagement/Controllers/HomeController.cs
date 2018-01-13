7898eb8fa89a3f67754818a37df040bb3dd4d934
Tom <tj_noronha@hotmail.com>
2018-01-13 23:55:38 +0000

    Remove unused pages and add Code button in navbar

--- WebNetworkManagement/Controllers/HomeController.cs
+++ WebNetworkManagement/Controllers/HomeController.cs
@@ -9,20 +9,6 @@ namespace WebNetworkManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult GetTweets()
         {
             return View("../Tutorial/GetTweets");
